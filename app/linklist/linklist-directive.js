6fb1a4b3b8a8d633c01ec101e8bb7cb14f9d14cb
Andrew Pyle <andy.pyle@gmail.com>
2015-07-14 21:49:17 -0700

    Added update functionality.

--- app/linklist/linklist-directive.js
+++ app/linklist/linklist-directive.js
@@ -8,17 +8,18 @@ var fb = new Firebase(FIREBASE_URI + 'links/');
         scope.loadingData = true;
         scope.isLoggedIn = (fireUser.isLoggedIn()) ? true:false;
 
-        scope.$watch('isLoggedIn', function(i,o){
-          scope.isLoggedIn = o;
-        });
-
+        // Grab list of links from Firebase if user is logged in.
         fb.on('value', function(data){
           if(fireUser.isLoggedIn()){
             scope.links = $firebaseArray(data.ref());
             scope.loadingData = false;
             scope.isLoggedIn = true;
+            scope.$watchCollection('links', function(i,o){
+              //scope.links = i;
+            });
+            scope.$apply();
           }
-        });
+        }); /// END LIST FETCH
 
         // Check if the current user owns the document. If so, return true.
         scope.ownsDocument = function(docId){
@@ -31,7 +32,7 @@ var fb = new Firebase(FIREBASE_URI + 'links/');
               scope.isOwner = (snap.val().createdBy.uid === userId);
             });
           return scope.isOwner;
-        }
+        } /// END CHECK OWNERSHIP FUNCTION
 
         // Delete link, only if you own the document.
         scope.linkDelete = function(linkId){
@@ -42,17 +43,107 @@ var fb = new Firebase(FIREBASE_URI + 'links/');
         });
 
         var verifyDelete = confirm('Delete link: ' + scope.linkTitle + '?');
-        if(verifyDelete && scope.ownsDocument(linkId)){
-          fbLinks.remove(function(er){
-            if(er)
-              console.error('Error: ' + er);
-            else
-              console.log('Link: ' + scope.linkTitle + ' deleted.');
-          });
-        } else {
-          console.error('Document not deleted.');
-        }
-      }
+          if(verifyDelete && scope.ownsDocument(linkId)){
+            fbLinks.remove(function(er){
+              if(er)
+                console.error('Error: ' + er);
+              else
+                console.log('Link: ' + scope.linkTitle + ' deleted.');
+            });
+          } else {
+            console.error('Document not deleted.');
+          }
+        } /// END DELETE FUNCTION
+
+        scope.linkTagsEditArray = null;
+        // Edit link if you own the document.
+        scope.linkEdit = function(linkId){
+          if(scope.ownsDocument(linkId)){
+            scope.editor = {};
+            scope.editing = this.$index;
+            scope.editorTags = [];
+
+            scope.checkLink = function(url, linkId){
+              var fbLinksCheck = new Firebase(FIREBASE_URI + 'links/');
+              scope.linkExists = null;
+
+              fbLinksCheck.orderByChild('url')
+              .startAt(url)
+              .endAt(url)
+              .on('value', function(snap){
+                  if(snap.val() !== null){
+                    scope.linkExists = (snap.val()) ? true:false;
+                    if(snap.val()[linkId]){
+                      scope.linkSamePost = (snap.val()[linkId]['url'] === url);
+                    }
+                  }
+              });
+
+            };
+
+
+            scope.refreshTags = function(tags){
+              scope.editorTags = angular.copy(tags);
+            }
+
+            scope.refreshTitle = function(title){
+              scope.editTitle = angular.copy(title);
+            }
+
+            scope.removeTag = function(tagIndex){
+              scope.tempTags = angular.copy(scope.editorTags);
+
+              scope.tempTags.splice(tagIndex, 1);
+              scope.editorTags = angular.copy(scope.tempTags);
+              return scope.editorTags;
+            }
+
+            scope.editTags = function(tags){
+                //var splitter = ',' || ', ';
+                //tagsEditArray = tags.split(',');
+                console.log(tags);
+                return scope.editorTags = angular.copy(tags);
+
+                angular.forEach(tags, function(t){
+                  scope.editorTags = tags;
+                });
+            }
+
+            scope.saveEdit = function(linkId){
+              var fbLinkEdit = new Firebase(FIREBASE_URI + 'links/' + linkId);
+              var fbLinkEditArray = $firebaseArray(fbLinkEdit);
+
+
+              var toEdit = {
+                'url':this.editor.url,
+                'title':this.editor.title,
+                'description':this.editor.description,
+                'tags':this.editorTags
+              };
+
+              //var linkChecker = scope.checkLink(this.editor.url);
+              //var linkCheckerWithId = linkChecker[linkId];
+
+              //console.log(linkCheckerWithId.url);
+              scope.urlExists = (scope.linkSamePost != scope.linkExists)
+              scope.urlSame = scope.linkExists;
+
+              if((scope.urlExists) || (scope.urlSame)){
+                fbLinkEdit.update(toEdit);
+                console.log('Successfully updated.');
+                scope.linkTagsEditArray = [];
+                scope.editing = false;
+              } else {
+                console.error('Link already exists. Cannot add to db.');
+              }
+            }
+          } else {
+            console.error('Unable to edit.');
+          }
+
+
+        } /// END EDIT FUNCTION
+
     }
   }
 });
